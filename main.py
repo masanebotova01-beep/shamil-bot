52f96cb49f1355240d3b706880f8cc47aad88b4e
masanebotova01-beep <masanebotova01@gmail.com>
2026-05-24 12:45:22 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+from aiogram import Bot, Dispatcher
+
+# Вставь сюда свой НОВЫЙ токен (внутри кавычек)
+API_TOKEN = '8940866107:AAFFRggxufY1r9_c52jS4JKNhaYhNyzUVn4'
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+# ... дальше остальной код твоего бота ...
